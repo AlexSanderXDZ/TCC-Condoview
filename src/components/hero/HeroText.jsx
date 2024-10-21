faec512dc38e48afa94a0e94232c8105a29ef4da
Havz <adryan.alex16@gmail.com>
2024-10-20 22:12:42 -0300

    edit do heros

--- src/components/hero/HeroText.jsx
+++ src/components/hero/HeroText.jsx
@@ -3,17 +3,16 @@ import { motion } from 'framer-motion';
 
 const HeroText = () => {
   // URL do arquivo beta do app
-  const betaAppUrl = 'https://github.com/AlexSanderXDZ/Beta-Condoapp/raw/refs/heads/main/Condoapp-Beta.apk'; 
+  const betaAppUrl = 'https://example.com/path/to/beta-app.apk'; 
 
   return (
     <motion.div
-      className="w-full lg:w-1/2 px-4 lg:px-0 my-10"
-      style={{ marginLeft: '-20px' }} // Ajuste a margem esquerda conforme necessário
+      className="w-full lg:w-1/2 px-5 lg:px-0 lg:ml-20 my-10"
       initial={{ y: 300 }}
       animate={{ y: 0 }}
       transition={{ delay: 0.5, stiffness: 100, type: 'spring' }}
     >
-      <div className="space-y-8 mb-8">
+      <div className="space-y-5 mb-5">
         <h1 className="text-[#141414] font-bold hero-text text-4xl lg:text-5xl leading-[30px] lg:leading-[45px] tracking-[-2%] lg:tracking-[-2.5%]">
           Sistema e Aplicativo para Administração de Condomínios
         </h1>
@@ -27,7 +26,7 @@ const HeroText = () => {
           download 
           className="inline-block px-6 py-3 bg-[#6D28D9] text-white font-semibold text-lg rounded-lg shadow-md hover:bg-[#4C1D95] transition duration-300"
         >
-          Baixe nossa Versão Beta <br /> para Android
+          Baixe Nossa Versão Beta para Android
         </a>
       </div>
     </motion.div>
