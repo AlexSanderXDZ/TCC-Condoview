f9ff7f8b5c209584fa17699b90ec6f7087250659
Adryan Alex. <adryan.alex16@gmail.com>
2024-11-23 18:20:45 -0300

    att model

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -9,25 +9,29 @@ const Header = () => {
 
   return (
     <div
-      className="header flex justify-between items-center w-full p-5 lg:px-20 "
+      className="header flex justify-between items-center w-full p-5 lg:px-20 bg-[#C5B4E3] shadow-md sticky top-0 z-50"
       id="logo"
     >
       <Link className="flex space-x-2 items-center" to={"/"}>
-       {/*<img src="" alt="Condoview Logo" className="w-8 lg:w-10" /> */} 
-        <h1 className="text-xl sm:text-4xl font-bold">CondoView</h1>
+        <h1 className="text-xl sm:text-4xl font-bold text-[#141414]">CondoView</h1>
       </Link>
 
-      <div onClick={handleClick} className="lg:hidden z-10 text-2xl sm:text-4xl">
+      {/* Botão do menu hambúrguer */}
+      <div
+        onClick={handleClick}
+        className="lg:hidden z-50 text-2xl sm:text-4xl text-[#141414] cursor-pointer transition-transform transform hover:scale-110"
+      >
         {!nav ? <FaBars /> : <FaTimes className="hover:text-red-500" />}
       </div>
 
-      <div className="w-3/5 justify-between items-center hidden lg:flex ">
+      {/* Menu Desktop */}
+      <div className="w-3/5 justify-between items-center hidden lg:flex">
         <div className="w-2/3 flex justify-between text-black text-lg font-semibold">
           <ScrollLink
             to="Início"
             smooth={true}
             duration={500}
-            className="cursor-pointer hover:scale-105"
+            className="cursor-pointer hover:scale-105 transition-transform"
           >
             Início
           </ScrollLink>
@@ -35,7 +39,7 @@ const Header = () => {
             to="sobre"
             smooth={true}
             duration={500}
-            className="cursor-pointer hover:scale-105"
+            className="cursor-pointer hover:scale-105 transition-transform"
           >
             Sobre
           </ScrollLink>
@@ -43,7 +47,7 @@ const Header = () => {
             to="sobra"
             smooth={true}
             duration={500}
-            className="cursor-pointer hover:scale-105"
+            className="cursor-pointer hover:scale-105 transition-transform"
           >
             Sobre o app
           </ScrollLink>
@@ -51,61 +55,59 @@ const Header = () => {
             to="proposta"
             smooth={true}
             duration={500}
-            className="cursor-pointer hover:scale-105"
+            className="cursor-pointer hover:scale-105 transition-transform"
           >
             Propostas
           </ScrollLink>
         </div>
       </div>
 
-      {/* Mobile menu */}
+      {/* Menu Mobile */}
       <ul
-        className={
-          !nav
-            ? "hidden"
-            : "absolute top-20 left-0 w-full h-screen flex bg-[#A] z-40 flex-col justify-center items-center"
-        }
+        className={`fixed top-0 left-0 w-full h-screen bg-[#6D28D9] text-white flex flex-col items-center justify-center transform ${
+          nav ? "translate-x-0" : "-translate-x-full"
+        } transition-transform duration-300 ease-in-out z-40`}
       >
-        <li className="py-8 text-6xl">
+        <li className="py-8 text-4xl font-semibold hover:scale-105 transition-transform">
           <ScrollLink
             to="Início"
             smooth={true}
             duration={500}
             onClick={handleClick}
-            className="hover:scale-105 cursor-pointer"
+            className="cursor-pointer"
           >
             Início
           </ScrollLink>
         </li>
-        <li className="py-8 text-6xl">
+        <li className="py-8 text-4xl font-semibold hover:scale-105 transition-transform">
           <ScrollLink
             to="sobre"
             smooth={true}
             duration={500}
             onClick={handleClick}
-            className="hover:scale-105 cursor-pointer"
+            className="cursor-pointer"
           >
             Sobre
           </ScrollLink>
         </li>
-        <li className="py-8 text-6xl">
+        <li className="py-8 text-4xl font-semibold hover:scale-105 transition-transform">
           <ScrollLink
             to="sobra"
             smooth={true}
             duration={500}
             onClick={handleClick}
-            className="hover:scale-105 cursor-pointer"
+            className="cursor-pointer"
           >
             Sobre o app
           </ScrollLink>
         </li>
-        <li className="py-8 text-6xl">
+        <li className="py-8 text-4xl font-semibold hover:scale-105 transition-transform">
           <ScrollLink
             to="Proposta"
             smooth={true}
             duration={500}
             onClick={handleClick}
-            className="hover:scale-105 cursor-pointer"
+            className="cursor-pointer"
           >
             Propostas
           </ScrollLink>
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -5,15 +5,24 @@ import img from "../assets/imgs/conda.png";
 
 const Hero = () => {
   return (
-    <div id="inicio" className='bg-[#C5B4E3] flex flex-col lg:justify-between lg:h-screen w-screen'>
+    <div id="inicio" className="bg-[#C5B4E3] flex flex-col lg:justify-between lg:min-h-screen w-screen">
       <Header />
-      <div className='lg:flex lg:justify-between'>
-        <HeroText />
-        <div className='lg:w-5/9 flex justify-end '> 
-          <img src={img} className='w-full h-auto' alt="Imagem Hero" />
+      <div className="lg:flex lg:justify-between lg:items-center lg:gap-10">
+        {/* Primeira imagem (aparece em telas menores, some em maiores) */}
+        <div className="lg:w-5/9 flex justify-end"> 
+          <img src={img} className="block lg:hidden w-full h-auto" alt="Imagem Hero" />
+        </div>
+
+        {/* HeroText com margem personalizada */}
+        <HeroText className="lg:mt-10" />
+
+        {/* Segunda imagem (some em telas menores, aparece em maiores) */}
+        <div className="lg:w-5/9 flex justify-end"> 
+          <img src={img} className="hidden lg:block w-full h-auto" alt="Imagem Hero" />
         </div>
       </div>
     </div>
   );
-}
+};
+
 export default Hero;
